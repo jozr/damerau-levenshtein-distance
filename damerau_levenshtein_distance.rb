bfb4a3be7cffe11ba0132a5ccb9fecb590308624
Josephine Wright <jozright@gmail.com>
2018-10-07 16:25:44 +0200

    Extract matrix builder

--- damerau_levenshtein_distance.rb
+++ damerau_levenshtein_distance.rb
@@ -1,13 +1,6 @@
 require "matrix"
 require "pry"
 
-# Allow positions to be mutated
-class Matrix
-  def []=(i, j, value)
-    @rows[i][j] = value
-  end
-end
-
 # Still just levenshtein
 class DamerauLevenshtein
   def initialize(string_a, string_b)
@@ -15,7 +8,47 @@ class DamerauLevenshtein
     @string_b = string_b
     @a_length = string_a.length
     @b_length = string_b.length
-    @matrix   = Matrix.build(a_length + 1, b_length + 1) do |row, col| # extract to own method
+    @matrix   = mutable_matrix(a_length, b_length)
+  end
+
+  def distance
+    return a_length if b_length.zero?
+    return b_length if a_length.zero?
+
+    (1..b_length).each do |j|
+      (1..a_length).each do |i|
+        matrix[i, j] = cost(i, j)
+      end
+    end
+
+    matrix[a_length, b_length]
+  end
+
+  private
+
+  def cost(i, j)
+    if same_character?(i, j)
+      matrix[i - 1, j - 1]
+    else
+      min_operation_value(i, j) + 1
+    end
+  end
+
+  def same_character?(i, j)
+    string_a[i - 1] == string_b[j - 1]
+  end
+
+  def min_operation_value(i, j)
+    binding.pry
+    [
+      matrix[i - 1, j],    # Deletion:     Look left in the matrix. It costs + 1 more than matrix[i-1, j]
+      matrix[i, j - 1],    # Insertion:    Look directly above the current entry in matrix. Costs +1.
+      matrix[i - 1, j - 1] # Substitution: Completely substitute the character. This also costs +1 more  than matrix[j-1, i-1]
+    ].min
+  end
+
+  def mutable_matrix(a_length, b_length)
+    Matrix.build(a_length + 1, b_length + 1) do |row, col|
       if row.zero?
         col
       elsif col.zero?
@@ -26,40 +59,12 @@ class DamerauLevenshtein
     end
   end
 
-  def distance
-    return a_length if b_length.zero?
-    return b_length if a_length.zero?
-
-    (1..b_length).each do |j|
-      (1..a_length).each do |i|
-        matrix[i, j] = costs_for_step(i, j)
-      end
-      end
-
-    matrix[a_length, b_length]
-  end
-
-  private
-
   attr_reader :string_a, :string_b, :a_length, :b_length, :matrix
+end
 
-  def same_character_for_both_words_is_added?(i, j)
-    string_a[i - 1] == string_b[j - 1]
-  end
-
-  def obtain_minimal_value_from_neighbors(i, j)
-    [
-      matrix[i - 1, j],    # Look left in the matrix. This would be a "deletion". It costs + 1 more than matrix[i-1, j]]
-      matrix[i, j - 1],    # Look directly above the current entry in matrix. This would correspond to an insertion which costs +1 additionally
-      matrix[i - 1, j - 1] # Completely substitute the character, this also costs +1 more  than matrix[j-1, i-1]
-    ].min
-  end
-
-  def costs_for_step(i, j)
-    if same_character_for_both_words_is_added?(i, j)
-      matrix[i - 1, j - 1]
-    else
-      obtain_minimal_value_from_neighbors(i, j) + 1
-    end
+# Allow positions to be mutated
+class Matrix
+  def []=(i, j, value)
+    @rows[i][j] = value
   end
 end
